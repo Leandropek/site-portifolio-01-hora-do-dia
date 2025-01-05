9212ce3f6bd45d7dee3f9385fbb4cf89b26910a1
Leandro <90192692+Leandropek@users.noreply.github.com>
2025-01-05 18:02:22 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,25 +1,63 @@
+function obterDataAtual() {
+    const data = new Date();
 
-let data = new Date();
-let hora = data.getHours()
-let minutos = data.getMinutes()
+    const diasDaSemana = [
+        "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
 
-const titulo = document.querySelector("h2");
-const imagem = document.querySelector("img");
+    const meses = [
+        "janeiro", "fevereiro", "março", "abril", "maio", "junho",
+        "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+    ];
 
-titulo.innerText = `Agora são ${hora} horas e ${minutos} minutos.`;
+    const semana = diasDaSemana[data.getDay()];
+    const dia = String(data.getDate()).padStart(2, '0'); 
+    const mes = meses[data.getMonth()];
+    const ano = data.getFullYear();
 
-if (hora < 12) { 
-    imagem.src = "assets/img/dia.jpg";
-    imagem.alt = "Dia";
-    document.body.style.backgroundColor = "#8DCAE6"
-
-} else if (hora < 18) {
-    imagem.src = "assets/img/tarde.jpg";
-    imagem.alt = "Tarde";
-    document.body.style.backgroundColor = "#F3C776"
-
-} else {
-    imagem.src = "assets/img/noite.jpg";
-    imagem.alt = "Noite";
-    document.body.style.backgroundColor = "#1D3044"
+    return `${semana}, ${dia} de ${mes} de ${ano}.`;
 }
+
+
+// Inserindo a data atual no HTML
+document.addEventListener("DOMContentLoaded", () => {
+    const elementoTexto = document.querySelector(".info-texto");
+    if (elementoTexto) {
+        elementoTexto.innerText = obterDataAtual();
+    }
+
+
+});
+
+
+
+
+
+const titulo = document.querySelector(".hora");
+const imagem = document.querySelector(".img");
+
+function atualizar() {
+    const data = new Date(); // Obtém a data e hora atuais
+    const hora = String(data.getHours()).padStart(2, '0');
+    const minutos = String(data.getMinutes()).padStart(2, '0');
+    const segundos = String(data.getSeconds()).padStart(2, '0');
+
+    titulo.innerText = `${hora}:${minutos}:${segundos}`;
+
+    if (hora < 12) { 
+        imagem.src = "assets/img/dia.jpg";
+        imagem.alt = "Dia";
+        document.body.style.background = "#8DCAE6";
+    } else if (hora < 18) {
+        imagem.src = "assets/img/tarde.jpg";
+        imagem.alt = "Tarde";
+        document.body.style.background = "#F3C776";
+    } else {
+        imagem.src = "assets/img/noite.jpg";
+        imagem.alt = "Noite";
+        document.body.style.background = "#1D3044";
+    }
+}
+
+setInterval(atualizar, 1000);
+
+atualizar();
