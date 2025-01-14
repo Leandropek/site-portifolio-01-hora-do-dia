e29832a4cc10735591e7b4b637b5e1056f37c0e9
Leandro <leandropek@hotmail.com>
2025-01-13 23:27:13 -0300

    remodelei

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+function setClock() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondAngle = (seconds / 60) * 360 + 90;
+    const minuteAngle = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+    const hourAngle = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+
+    document.querySelector('.segundo').style.transform = `rotate(${secondAngle}deg)`;
+    document.querySelector('.minuto').style.transform = `rotate(${minuteAngle}deg)`;
+    document.querySelector('.hora').style.transform = `rotate(${hourAngle}deg)`;
+}
+
+setInterval(setClock, 1000);
+setClock();
+
 function obterDataAtual() {
     const data = new Date();
 
@@ -34,13 +53,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-
 const titulo = document.querySelector(".hora-digital");
 const imagem = document.querySelector(".imagem");
 
 function atualizar() {
+
     const data = new Date();
-    const hora = String(data.getHours()).padStart(2, '0');
+    const hora = String(data.getHours()).padStart(2, '0'); 
     const minutos = String(data.getMinutes()).padStart(2, '0');
     const segundos = String(data.getSeconds()).padStart(2, '0');
 
@@ -49,7 +68,7 @@ function atualizar() {
     if (hora < 12) { 
         imagem.src = "assets/img/dia.jpg";
         imagem.alt = "Dia";
-        document.body.style.background = "#8DCAE6";
+        document.body.style.background = "#94d8f5";
     } else if (hora < 18) {
         imagem.src = "assets/img/tarde.jpg";
         imagem.alt = "Tarde";
